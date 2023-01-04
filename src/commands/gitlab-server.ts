0b20101e6303610f823a11e2d7b4911f57feb261
Mike Urbanski <murbanski@paloaltonetworks.com>
2023-01-04 15:28:53 -0600

    fix gitlab server source info

--- src/commands/gitlab-server.ts
+++ src/commands/gitlab-server.ts
@@ -44,7 +44,7 @@ export default class GitlabServer extends Gitlab {
 
         const serverUrl = getServerUrl(flags.hostname, flags.port, flags.protocol);
         const baseUrl = `${serverUrl}/api/v4`;
-        const sourceInfo = this.getSourceInfo(flags.token, baseUrl, SourceType.GithubServer);
+        const sourceInfo = Gitlab.getSourceInfo(flags.token, baseUrl, SourceType.GitlabServer);
 
         const apiManager = new GitlabServerApiManager(sourceInfo, flags['ca-cert']);
         const runner = new GitlabServerRunner(sourceInfo, flags, apiManager);
