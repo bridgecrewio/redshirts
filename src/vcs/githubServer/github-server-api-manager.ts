10953e5546710d952fe67778f21016de561c7c33
Mike Urbanski <murbanski@paloaltonetworks.com>
2023-01-06 14:59:01 -0600

    fix: handle 404 from rate limit endpoint when limiting is disabled

--- src/vcs/githubServer/github-server-api-manager.ts
+++ src/vcs/githubServer/github-server-api-manager.ts
@@ -1,4 +1,6 @@
-import { AxiosRequestConfig } from 'axios';
+import { AxiosError, AxiosRequestConfig } from 'axios';
+import { RateLimitStatus } from '../../common/types';
+import { LOGGER } from '../../common/utils';
 import { GithubApiManager } from '../github/github-api-manager';
 
 export class GithubServerApiManager extends GithubApiManager {
@@ -7,5 +9,30 @@ export class GithubServerApiManager extends GithubApiManager {
             Accept: 'application/vnd.github+json',
             Authorization: `Bearer ${this.sourceInfo.token}`
         });
+    }
+
+    async checkRateLimitStatus(): Promise<RateLimitStatus | undefined> {
+
+        // the github server rate limit endpoint returns a 404 if rate limiting is not enabled
+
+        const config: AxiosRequestConfig = {
+            url: this.rateLimitEndpoint,
+            method: 'GET',
+        };
+
+        try {
+            const response = await this.axiosInstance.request(config);
+            if (this.logApiResponses) {
+                LOGGER.debug('', { response });
+            }
+
+            return this.getRateLimitStatus(response);
+        } catch (error) {
+            if (error instanceof AxiosError && error.response?.data.message === 'Rate limiting is not enabled.') {
+                return undefined;
+            }
+
+            throw error;
+        }
     }
 }
